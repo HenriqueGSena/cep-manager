7854f15c78f613987f7b4d4ac7c54838f847e089
henrique.sena <carloshenrique3250@gmail.com>
2025-03-21 22:48:31 -0300

    :tada:Initial commit

---
+++ cepManagerServer/src/main/java/com/br/cepManager/CepManagerApplication.java
@@ -0,0 +1,13 @@
+package com.br.cepManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepManagerApplication.class, args);
+	}
+
+}
